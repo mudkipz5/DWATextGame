b899add2d3b079c25ae647ce4e027dfc17f762fa
josefc <josefcitrine@gmail.com>
2015-01-17 18:15:40 +0000

    Cleaned up the project
    Did quite a lot over the last 2 weeks. Streamlined the Dedalus engine
    into one file, fixed a few bugs, changed the look, expanded the story
    and moved the story into a data folder

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ DWATextGame
 
 This is the code for the Doctor Whooves text adventure. Right now we're in development
 
-The story is found inside story.dedlee and uses the dedlee format from dedalus.
-
 View the latest version at http://piabwiki.com/dwa-text/
+
+Engine
+======
+The game runs on a modified version of the [Dedalus](https://github.com/pistacchio/Dedalus) engine. One of the big differences is that the story is not contained within the index file. The story can be accessed in the data folder as story.dat. This is just a HTML file that gets loaded into the story div using javascript.
---
+++ js/app.js
@@ -0,0 +1,33 @@
+/* DWA Text Adventure
+Copyright (C) 2015 Pony in a Box Productions
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+Full license: http://www.gnu.org/licenses/gpl-2.0.html */
+
+$(function () {
+	$.get( "data/story.dat", function( data ) {
+		$( "#story" ).html(data);
+		new DedalusWeb({
+			domSource         : $('#story'),
+			domTarget         : $('#host'),
+			titleTarget       : $('#title'),
+			inventoryTarget   : $('#inventoryHost'),
+			interactionTarget : $('#interactionHost'),
+			undoTarget        : $('#undoHost'),
+			undoStageTarget   : $('#undoStageHost'),
+			saveTarget        : $('#saveHost'),
+			restoreTarget     : $('#restoreHost'),
+			resetTarget       : $('#resetHost'),
+		});
+		$('#version').text("v0.2 Alpha");
+	});
+});
